61e951923f41ab807eae391d947febb376dfc8c3
Stephan Elondo <selondo@Stephans-MacBook-Pro.local>
2020-01-01 21:55:57 -0500

    implemented react-router

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i className="fas fa-code"></i> Blog-Muket</a>
+        <Link to="/"><i className="fas fa-code"></i> Blog-Muket </Link>
       </h1>
       <ul>
         <li><a href="profiles.html">Blogs</a></li>
-        <li><a href="register.html">Sign Up</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li><Link to="register">Sign Up </Link></li>
+        <li><Link to="login">Login</Link> </li>
       </ul>
     </nav>
   )
